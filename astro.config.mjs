48178dcd9b9c8028859b3b6f71b9f8faa2d4ff81
Hossien Mohmmadian <hosseinhunta@hotmail.com>
2023-12-12 15:42:27 +0330

    out server

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import svelte from '@astrojs/svelte';
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()],
-  output: 'static',
-  adapter: vercel()
+	integrations: [tailwind(), svelte()],
+	output: 'server',
+	adapter: vercel(),
 });
